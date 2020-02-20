cd9ef2855ab821bdd650277ccc07866f5f288431
mangoh <t.schmidt@md-network.de>
2020-02-19 16:10:58 -0800

    Additional changes in basic BLE test setup

--- ComponentYellowBLEScanner/main.c
+++ ComponentYellowBLEScanner/main.c
@@ -1,6 +1,13 @@
 #include "legato.h"
+#include "interfaces.h"
+#include "DBusBLEScanner.h"
 
 COMPONENT_INIT { 
+//    le_result_t result;
+
     LE_INFO("Starting Yellow BLE Scanner");
-    exit(0);
+
+    yel_ble_setupScan();
+
+
 }
