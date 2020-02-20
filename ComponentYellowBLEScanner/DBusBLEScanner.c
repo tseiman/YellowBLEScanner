7c086a7dbb936d63a5f7d2863d067062d55313f0
t.schmidt@md-network.de <t.schmidt@md-network.de>
2020-02-19 16:44:52 -0800

    Updated Readme

--- ComponentYellowBLEScanner/DBusBLEScanner.c
+++ ComponentYellowBLEScanner/DBusBLEScanner.c
@@ -51,136 +51,9 @@ static gboolean LegatoFdHandler (GIOChannel *source, GIOCondition condition, gpo
     return TRUE;
 }
 
-/*
-
-static void AllAttributesFoundHandler() {
-    AppState = APP_STATE_SAMPLING;
-    GError *error = NULL;
-
-    LE_INFO("All attributes Found");
-}
-*/
-/*
-static bool TryProcessAsService( GDBusObject *object, const gchar *objectPath){
-
-    bool found = false;
-    BluezGattService1 *serviceProxy = BLUEZ_GATT_SERVICE1(g_dbus_object_get_interface(object, "org.bluez.GattService1"));
-    if (serviceProxy == NULL)
-        return false;
-
-    const gchar *serviceUuid = bluez_gatt_service1_get_uuid(serviceProxy);
-    for (size_t i = 0; i < NUM_ARRAY_MEMBERS(Services); i++) {
-        if (g_strcmp0(serviceUuid, Services[i].uuid) == 0) {
-            LE_DEBUG("%s service found at: %s", Services[i].name, objectPath);
-            LE_ASSERT(Services[i].objectPath == NULL);
-            Services[i].objectPath = g_strdup(objectPath);
-            found = true;
-            break;
-        }
-    }
-    g_clear_object(&serviceProxy);
-
-LE_INFO("TryProcessAsService");
-    return found;
-} */
-/*
-static bool TryProcessAsCharacteristic( GDBusObject *object, const gchar *objectPath) {
-
-    BluezGattCharacteristic1 *characteristicProxy = BLUEZ_GATT_CHARACTERISTIC1(g_dbus_object_get_interface(object, "org.bluez.GattCharacteristic1"));
-    if (characteristicProxy == NULL)
-        return false;
-    
-    const gchar *characteristicUuid = bluez_gatt_characteristic1_get_uuid(characteristicProxy);
-
-    for (size_t serviceIdx = 0; serviceIdx < NUM_ARRAY_MEMBERS(Services); serviceIdx++) {
-        const bool charInService = (
-            g_strcmp0( bluez_gatt_characteristic1_get_service(characteristicProxy),
-                Services[serviceIdx].objectPath) == 0);
-        if (charInService) {
-            for (const struct Characteristic *characteristic = Services[serviceIdx].characteristics; characteristic->uuid != NULL; characteristic++) {
-                if (g_strcmp0(characteristic->uuid, characteristicUuid) == 0) {
-                    LE_ASSERT(*(characteristic->proxy) == NULL);
-                    LE_DEBUG("service %s characteristic %s found at %s",Services[serviceIdx].name, characteristic->uuid, objectPath);
-                    *(characteristic->proxy) = characteristicProxy;
-                    return true;
-                }
-            }
-            break;
-        }
-    }
-
-    g_clear_object(&characteristicProxy);
- 
-    LE_INFO("TryProcessAsCharacteristic");
-    return false;
-}
-   */
-/*
-static void TryProcessAsAttribute( GDBusObject *object, const gchar *devicePath){
-     LE_DEBUG("------->>>> TryProcessAsAttribute()");
-
-/ *    gchar *unknownObjectPath = NULL;
-    g_object_get(object, "g-object-path", &unknownObjectPath, NULL);
-    const bool childOfDevice = g_str_has_prefix(unknownObjectPath, devicePath);
-    if (!childOfDevice)  goto done;
-
-
-    bool missingCharacteristic = false;
-    if (TryProcessAsService(object, unknownObjectPath)) {
-        // Services are discovered before their attributes, so there's no point in checking if all
-        // attributes are found.
-    } else if (TryProcessAsCharacteristic(object, unknownObjectPath)) {
-        for (size_t serviceIdx = 0; serviceIdx < NUM_ARRAY_MEMBERS(Services) && !missingCharacteristic; serviceIdx++) {
-            for (const struct Characteristic *characteristic = Services[serviceIdx].characteristics; characteristic->uuid != NULL; characteristic++) {
-                if (*(characteristic->proxy) == NULL) {
-                    missingCharacteristic = true;
-                    LE_DEBUG("Found missing characteristic: %s:%s", Services[serviceIdx].name, characteristic->uuid);
-                    break;
-                }
-            }
-        }
-
-        if (!missingCharacteristic) {
-            LE_DEBUG("Found all attributes");
-            AllAttributesFoundHandler();
-        }
-    }
-
-done:
-    g_free(unknownObjectPath);
-    * /
-    LE_INFO("TryProcessAsAttribute");
-}
-*/
-/*
-static BluezDevice1 *TryCreateSensorTagDeviceProxy(GDBusObject *object){
- LE_DEBUG("------->>>> TryCreateSensorTagDeviceProxy()");
-    BluezDevice1* dev = BLUEZ_DEVICE1(g_dbus_object_get_interface(object, "org.bluez.Device1"));
-    if (dev != NULL)    {
-        const gchar *deviceName = bluez_device1_get_name(dev);
-        const gchar *deviceAddr = bluez_device1_get_address(dev);
-        const gchar *deviceAddrType = bluez_device1_get_address_type(dev);
-        const gint16 deviceRSSI = bluez_device1_get_rssi(dev);
-
-
-/ *        if (g_strcmp0("CC2650 SensorTag", deviceName) != 0 && g_strcmp0("CC1350 SensorTag", deviceName) != 0){
-            // Not a match, so dispose of the object
-            g_clear_object(&dev);
-        } * /
-LE_INFO("-----> found device addr: %s (%s), with RSSI %d, and name: %s", deviceAddr,deviceAddrType, deviceRSSI, deviceName);
-    g_clear_object(&dev);
-
-
-    }
-    return dev;
-}
-*/
 
 GType BluezProxyTypeFunc(GDBusObjectManagerClient *manager, const gchar *objectPath, const gchar *interfaceName, gpointer userData) {
 
- //       LE_DEBUG("------->>>> BluezProxyTypeFunc()");
-
-//    LE_DEBUG("proxy ctor: path=%s, intf=%s", objectPath, interfaceName);
     if (interfaceName == NULL) 
         return g_dbus_object_proxy_get_type();
     
@@ -201,30 +74,6 @@ GType BluezProxyTypeFunc(GDBusObjectManagerClient *manager, const gchar *objectP
 }
 
 
-/*
-static void SensorTagFoundHandler(void)
-{
-        LE_DEBUG("------->>>> SensorTagFoundHandler()");
-//    GError *error = NULL;
-    LE_DEBUG("SensorTag found - calling Device1.Connect()");
-  / *  AppState = APP_STATE_SEARCHING_FOR_ATTRIBUTES;
-    bluez_device1_call_connect_sync(SensorTagDeviceInterface, NULL, &error);
-    LE_FATAL_IF(error != NULL, "Failed to connect to SensorTag - %s", error->message);
-
-    gchar *devicePath = NULL;
-    g_object_get(SensorTagDeviceInterface, "g-object-path", &devicePath, NULL);
-    GList *bluezObjects = g_dbus_object_manager_get_objects(BluezObjectManager);
-    for (GList *node = bluezObjects;
-         node != NULL;
-         node = node->next)
-    {
-        GDBusObject *obj = node->data;
-        TryProcessAsAttribute(obj, devicePath);
-    }
-    g_list_free_full(bluezObjects, g_object_unref);
-    g_free(devicePath);* /
-}
-*/
 
 static void BeginSensorTagSearch(void) {
     LE_DEBUG("------->>>> BeginSensorTagSearch()");
@@ -292,39 +141,6 @@ static void AdapterFoundHandler(void){
 
 
 static void BluezObjectAddedHandler(GDBusObjectManager *manager, GDBusObject *object, gpointer userData) {
-//     LE_DEBUG("------->>>> BluezObjectAddedHandler()");
-/*    switch (AppState)
-    {
-    case APP_STATE_SEARCHING_FOR_ADAPTER:
-        AdapterInterface = BLUEZ_ADAPTER1(g_dbus_object_get_interface(object, "org.bluez.Adapter1"));
-        if (AdapterInterface != NULL) {
-            AdapterFoundHandler();
-        }
-        break;
-
-    case APP_STATE_SEARCHING_FOR_SENSORTAG:
-        {
-            SensorTagDeviceInterface = TryCreateSensorTagDeviceProxy(object);
-            if (SensorTagDeviceInterface != NULL) {
-                SensorTagFoundHandler();
-            }
-        }
-        break;
-
-    case APP_STATE_SEARCHING_FOR_ATTRIBUTES:
-        {
-            gchar *devicePath = NULL;
-            g_object_get(SensorTagDeviceInterface, "g-object-path", &devicePath, NULL);
-            TryProcessAsAttribute(object, devicePath);
-            g_free(devicePath);
-        }
-        break;
-
-    default:
-        LE_DEBUG("Received \"object-added\" signal - object_path=%s, state=%d", g_dbus_object_get_object_path(object), AppState);
-        break;
-    }
-    */
         LE_DEBUG("Received \"object-added\" signal - object_path=%s, state=%d", g_dbus_object_get_object_path(object), AppState);
 
     BluezDevice1* dev = BLUEZ_DEVICE1(g_dbus_object_get_interface(object, "org.bluez.Device1"));
@@ -336,12 +152,9 @@ static void BluezObjectAddedHandler(GDBusObjectManager *manager, GDBusObject *ob
         const gint16 deviceRSSI = bluez_device1_get_rssi(dev);
 
 
-/*        if (g_strcmp0("CC2650 SensorTag", deviceName) != 0 && g_strcmp0("CC1350 SensorTag", deviceName) != 0){
-            // Not a match, so dispose of the object
-            g_clear_object(&dev);
-        } */
-LE_INFO("-----> added device addr: %s (%s), with RSSI %d, and name: %s", deviceAddr,deviceAddrType, deviceRSSI, deviceName);
-    g_clear_object(&dev);
+
+        LE_INFO("-----> added device addr: %s (%s), with RSSI %d, and name: %s", deviceAddr,deviceAddrType, deviceRSSI, deviceName);
+        g_clear_object(&dev);
 
 
     }
